6a3abdfd2789993618cecb048fbbe4b8fe977f1b
Derek Lim <dereklim3006@gmail.com>
2024-12-23 13:21:44 -0500

    Create function that add book objects to library

--- javascript.js
+++ javascript.js
@@ -6,3 +6,32 @@ function Book(title, author, pages, read) {
   this.pages = pages
   this.read = read
 }
+
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read))
+}
+
+addBookToLibrary(
+  'The Pragmatic Programmer',
+  'David Thomas, Andrew Hunt',
+  352,
+  true
+)
+addBookToLibrary(
+  'Code Complete',
+  'Steve McConnell',
+  960,
+  false
+)
+addBookToLibrary(
+  'Git & GitHub Visual Guide',
+  'Ben Bloomfield, David Ocean, Atlas Skylark, Valeria Celis',
+  514,
+  true
+)
+addBookToLibrary(
+  'C Programming Language',
+  'Kernighan Brian W., Ritchie Dennis',
+  279,
+  true
+)
